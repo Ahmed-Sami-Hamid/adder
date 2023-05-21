b83633df6220f2856fdec1de532aa42ebb369dc4
ahmed sami hamid <ahmedsamihamid1@gmail.com>
2023-05-21 21:46:59 +0400

    just lunch the app

---
+++ src/views/Calculater.jsx
@@ -0,0 +1,10 @@
+import {useState} from 'react';
+
+const Calculater = () => {
+    const [check , setCheck] = useState({});
+  return (
+    <div>{{check}}</div>
+  )
+}
+
+export default Calculater;
